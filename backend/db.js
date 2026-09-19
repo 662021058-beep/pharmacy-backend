ea03b7bdc13475b69bf3e3851c1d98fa8d5e3f37
Macbook Air ของ Anaphat <anaphat-macbook@MacBook-Air--Anaphat.local>
2026-09-19 19:44:38 +0700

    Update db credentials to Clever Cloud

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'std.csit.scidi.tsu.ac.th',           // ชี้ไปที่ฐานข้อมูลในเซิร์ฟเวอร์มหาลัย
-  port: 3306,                  // Port มาตรฐานของ MySQL
-  user: 's662021058',          // Username ของมหาลัย
-  password: 'earth0918658003',   // Password ของมหาลัย
-  database: 'db662021058',      // ชื่อ Database ของมหาลัย (หรือ db662021058 ตามที่ระบุ)
+  host: 'brsnbcr7hjfgmgle84ag-mysql.services.clever-cloud.com',
+  port: 3306,
+  user: 'ufggp2rwcazaskip',
+  password: '46SYyN9CC0DcGhiLlQ5O',
+  database: 'brsnbcr7hjfgmgle84ag',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -14,9 +14,9 @@ const pool = mysql.createPool({
 
 pool.getConnection((err, connection) => {
   if (err) {
-    console.error('❌ เชื่อมต่อ MySQL มหาลัยไม่สำเร็จ:', err.message);
+    console.error('❌ เชื่อมต่อ Cloud MySQL ไม่สำเร็จ:', err.message);
   } else {
-    console.log('✅ เชื่อมต่อ MySQL มหาลัยสำเร็จเรียบร้อย!');
+    console.log('✅ เชื่อมต่อ Cloud MySQL สำเร็จเรียบร้อย!');
     connection.release();
   }
 });
