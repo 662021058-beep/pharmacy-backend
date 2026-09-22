ba60807e5dcce19f37e7f3fa75c09136ffd313a6
Macbook Air ของ Anaphat <anaphat-macbook@MacBook-Air--Anaphat.local>
2026-09-22 18:50:33 +0700

    Update db.js to use env variables

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
-// db.js
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',       // ใช้ IP ชี้ไปที่เครื่องตัวเอง
-  port: 3306,              // Port มาตรฐานของ MySQL ใน XAMPP
-  user: 'root',
-  password: '',            // XAMPP ค่าเริ่มต้นไม่มีรหัสผ่าน (ปล่อยว่าง)
-  database: 'lalita_pharmacy',
+  host: process.env.DB_HOST || process.env.MYSQL_ADDON_HOST || 'brsnbcr7hjfgmgle84ag-mysql.services.clever-cloud.com',
+  port: process.env.DB_PORT || process.env.MYSQL_ADDON_PORT || 3306,
+  user: process.env.DB_USER || process.env.MYSQL_ADDON_USER || 'ufggp2rwcazaskip',
+  password: process.env.DB_PASSWORD || process.env.MYSQL_ADDON_PASSWORD || '46SYyN9CC0DcGhiLLQ50',
+  database: process.env.DB_NAME || process.env.MYSQL_ADDON_DB || 'brsnbcr7hjfgmgle84ag',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -16,7 +16,7 @@ pool.getConnection((err, connection) => {
   if (err) {
     console.error('❌ เชื่อมต่อ MySQL ไม่สำเร็จ:', err.message);
   } else {
-    console.log('✅ เชื่อมต่อ MySQL ใน XAMPP สำเร็จเรียบร้อย!');
+    console.log('✅ เชื่อมต่อ MySQL สำเร็จเรียบร้อย!');
     connection.release();
   }
 });
